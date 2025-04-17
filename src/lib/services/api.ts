f6accd5de81334e171595812b0420fb614898ee8
vbasilioo <vbasilio2019@gmail.com>
2025-04-17 16:28:47 -0300

    chore(env): add env example and put api url in api.ts

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 import { getSession } from 'next-auth/react';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json',
     Accept: '*/*',
